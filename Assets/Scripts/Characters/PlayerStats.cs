b6ffb02436242d9ff1c9548bded09fe06caa60c9
setezerocinco <setezerocinco@gmail.com>
2022-01-02 16:13:46 +0100

    AddXP created to increase status.

--- Assets/Scripts/Characters/PlayerStats.cs
+++ Assets/Scripts/Characters/PlayerStats.cs
@@ -7,7 +7,7 @@ public class PlayerStats : MonoBehaviour
     [SerializeField] private int playerLevel = 1;
     [SerializeField] private int maxLevel = 50;
     [SerializeField] private int currentXP;
-    [SerializeField] private int[] xpForEachLevel;
+    [SerializeField] private int[] xpForNextLevel;
     [SerializeField] private int baseLevelXP = 100;
 
     [SerializeField] private int currentHP;
@@ -24,15 +24,44 @@ public class PlayerStats : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        xpForEachLevel = new int[maxLevel];
-        for (int i = 1; i < xpForEachLevel.Length; i++)
+        xpForNextLevel = new int[maxLevel];
+        for (int i = 1; i < xpForNextLevel.Length; i++)
         {
-            xpForEachLevel[i] = (int)(0.02f * i * i * i + 3.06 * i * i + 105.6f * i);
+            xpForNextLevel[i] = (int)(0.02f * i * i * i + 3.06 * i * i + 105.6f * i);
         }
     }
 
     // Update is called once per frame
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            AddXP(baseLevelXP);
+        }
+    }
+
+    public void AddXP(int amount)
+    {
+        currentXP += amount;
+        if (currentXP > xpForNextLevel[playerLevel])
+        {
+            currentXP -= xpForNextLevel[playerLevel];
+            playerLevel++;
+
+            maxHP = Mathf.FloorToInt(maxHP * 1.18f);
+            currentHP = maxHP;
+
+            maxMP = Mathf.FloorToInt(maxMP * 1.06f);
+            currentMP = maxMP;
+
+            if (playerLevel % 2 == 0)
+            {
+                dexterity++;
+            }
+            else
+            {
+                defence++;
+            }
+        }
     }
 }
